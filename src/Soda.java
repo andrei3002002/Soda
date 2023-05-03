7151057a136a57767a10d0aaedd6e8db80d05d10
Andrei Vilcu <andrei3002002@gmail.com>
2023-05-03 16:29:52 +0200

    Create class

---
+++ src/Soda.java
@@ -0,0 +1,17 @@
+public class Soda {
+  private String flavor;
+  private String Fanta;
+  private String Sprite;
+  private String Cola;
+
+  public Soda (String flavor){
+    this.flavor = flavor;
+  }
+  public String getFlavor(){
+    return flavor;
+  }
+
+  public void setFlavor(String flavor) {
+    this.flavor = flavor;
+  }
+}
