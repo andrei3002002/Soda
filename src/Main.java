88774e99b402c7278d166803f4650bf68aeca946
Andrei Vilcu <andrei3002002@gmail.com>
2023-05-03 16:31:27 +0200

    switch flavor

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+
+
 public class Main {
 
   public static void main(String[] args) {
     System.out.println("Hello world!");
+
+    Soda fanta = new Soda("Raspberries");
+    System.out.println("Old taste of Fanta: " + fanta.getFlavor());
+    fanta.setFlavor("Strawberry");
+    System.out.println("\tNew taste of Fanta: " + fanta.getFlavor());
+
+    System.out.println();
+
+    Soda cola  = new Soda("Vanilla");
+    System.out.println("Old taste of Cola: " + cola.getFlavor());
+    cola.setFlavor("Cherry");
+    System.out.println("\tNew taste of Cola: " + cola.getFlavor());
+
   }
 }
